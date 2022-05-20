5fee361d620329de8ec8ded0bd4ad7c8ef1e4694
Gamer1650 <Gamer1650@GAMER1650>
2022-05-20 14:31:39 -0400

    asd1

--- src/test/java/steps/XYZSmokeStep.java
+++ src/test/java/steps/XYZSmokeStep.java
@@ -7,7 +7,7 @@ public class XYZSmokeStep {
 	@Given("User navigates to login page")
 	public void user_navigates_to_login_page() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    System.out.println("User able to login...");
 	}
 
 }
