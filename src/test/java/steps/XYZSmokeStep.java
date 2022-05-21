3c889aa226463e1da3199d05d05569b92037a0bd
Addy <96358419+Badnaan@users.noreply.github.com>
2022-05-21 11:31:44 -0400

    Update XYZSmokeStep.java

--- src/test/java/steps/XYZSmokeStep.java
+++ src/test/java/steps/XYZSmokeStep.java
@@ -8,6 +8,8 @@ public class XYZSmokeStep {
 	public void user_navigates_to_login_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    System.out.println("User able to login...");
+		
+		//test changes
 	}
 
 }
